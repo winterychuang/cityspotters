1bf2f9fbbf61839772ea304ffa0708d66fa6e164
Winter Chuang <winterchuang@Winters-Laptop.local>
2026-08-15 10:29:27 -0700

    start server

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+
+app.set("view engine", "ejs")
+
+app.get("/", (req,res) => {
+    res.send("title page"); //tester
+})
+
+
+app.listen(PORT, () => 
+console.log("Server is running on port", PORT))
